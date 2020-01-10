3e045c2bff27f40ca75a386a4c6d46f4d08af8d9
Michal Witkowski <michalwitkowski89@gmail.com>
2020-01-10 10:22:22 +0100

    Renaming files + cosmetics

---
+++ Puzzler_11/src/Observable.merge.kt
@@ -0,0 +1,24 @@
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import java.lang.Thread.sleep
+
+// PUZZLER 11
+fun main(args: Array<String>) {
+    Observable
+        .merge(
+            Observable.fromCallable {
+                sleep(1000)
+                0
+            },
+            Observable.fromCallable {
+                sleep(1000)
+                1
+            }
+        )
+        .subscribeOn(Schedulers.io())
+        .subscribe {
+            println(it)
+        }
+
+    sleep(1500)
+}
